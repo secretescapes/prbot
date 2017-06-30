eb6dc20aaa908f6cf31517d0e23dbcc6e0a5c133
Josh Prendergast <josh.prendergast@secretescapes.com>
2017-06-30 16:18:50 +0100

    something

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,8 @@
 let handlebars = require('handlebars');
 let GitHubApi = require('github');
 let bluebird = require('bluebird');
+let mongoose = require('mongoose');
+let _ = require('lodash');
 
 let gh = new GitHubApi({
   debug: false,
@@ -14,7 +16,7 @@ let gh = new GitHubApi({
 
 // Templates
 let tableRow = handlebars.compile('<{{ html_url }}|{{ title }} [author: ' +
-  '{{ author.login }}, reward: {{ reward }}]>');
+    '{{ user.login }}, reward: {{ reward }}]>');
 
 // Configuration
 let config = {
@@ -22,6 +24,12 @@ let config = {
   REPO_NAME: process.env.REPO_NAME,
   REPO_OWNER: process.env.REPO_OWNER,
 };
+
+let Review = mongoose.model('Review', {
+  reward: {type: Number},
+  reviewee: {type: String},
+  reviewer: {type: String},
+});
 
 // Program
 module.exports = function (robot) {
@@ -32,30 +40,57 @@ module.exports = function (robot) {
     });
   };
 
-  let reward = function(pr) {
+  let getReward = function(pr) {
     return 100;
   };
 
-  robot.hear(/I want a PR for (\d+)/i, function(res) {
+  robot.hear(/I want a PR for (\d+)/i, function (res) {
     // TODO
   });
 
-  robot.hear(/what PRs need review/i, function(res) {
+  robot.hear(/what PRs need review/i, function (res) {
     authenticate();
+
     gh.pullRequests.getAll({
       repo: config.REPO_NAME,
       owner: config.REPO_OWNER,
       state: 'open',
-      sort: 'long-running',
-      direction: 'desc',
-      per_page: 10,
-    }).then(function(resp) {
+      per_page: 10, // TODO Pagination
+    }).then(function (resp) {
       let output = '';
-      resp.data.forEach(function(pr) {
-          pr.reward = reward(pr);
-          output += tableRow(pr) + '\n';
+      resp.data.forEach(function (pr) {
+        pr.reward = getReward(pr);
+        output += tableRow(pr) + '\n';
       });
-      robot.reply(output);
+      res.send(output);
     });
+  });
+
+  robot.router.get("/github-hook", function (req, res) {
+    let payload = req.body.payload;
+    if (payload.organization != config.REPO_OWNER || payload.repository != config.REPO_NAME) {
+      return; // Ignore
+    }
+
+    if (payload.action === 'closed' && payload.merged) {
+      authenticate();
+
+      gh.pullRequests.getReviews({
+        repo: config.REPO_NAME,
+        owner: config.REPO_OWNER,
+        number: payload.pull_request.id,
+        per_page: 100, // TODO Pagination
+      }).then(function (resp) {
+        var reward = getReward(payload.pull_request);
+        var people = _.uniq(_.map(resp.data, 'user.login'));
+
+        people.forEach(function (user) {
+          let r = new Review();
+          r.reward = reward;
+          r.reviewee = payload.pull_request.user.login;
+          r.reviewer = user;
+        });
+      });
+    }
   });
 };
